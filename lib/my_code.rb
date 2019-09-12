686ecab4717a04a42a9dbd457518db6861511ba5
Stacey Zander <staceyzander@gmail.com>
2019-09-12 03:13:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while array.length > i do
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new 
+end 
+
+
+def reduce(array, sv = nil)
+  i = 0 
+  while array.length > i do
+    
+  end
+end 
